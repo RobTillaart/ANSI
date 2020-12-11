13b0a7ba34b135f013ef7ea1d70bc25cdf69585d
Rob Tillaart <rob.tillaart@gmail.com>
2020-12-11 19:43:54 +0100

    Arduino ci (#5)
    * initial arduino-ci
    * add unit tests
    * implement flush() for due compile

--- ansi.h
+++ ansi.h
@@ -2,7 +2,7 @@
 //
 //    FILE: ansi.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.2
+// VERSION: 0.1.3
 // PURPOSE: Arduino library to send ANSI escape sequences
 //    DATE: 2020-04-28
 //     URL: https://github.com/RobTillaart/ANSI
@@ -16,9 +16,10 @@ public:
   ANSI(Stream * stream = &Serial);
 
   // Stream interface
-  int available();
-  int read();
-  int peek();
+  int  available();
+  int  read();
+  int  peek();
+  void flush()     { return; };  // placeholder to keep CI happy
 
   // CHAR MODES
   void normal()    { print("\033[0m"); };
@@ -70,7 +71,8 @@ public:
   };
 
   void clearScreen();
-  void clearLine(uint8_t clear=toEnd);
+  void clearLine(uint8_t clear = toEnd);
+
   void home()      { print("\033[H");  };
   
   void gotoXY(uint8_t x, uint8_t y);
