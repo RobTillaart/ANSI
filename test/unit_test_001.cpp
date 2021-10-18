70fe9b0dc597e1489ab81257456b2ecd6a82be44
rob tillaart <rob.tillaart@gmail.com>
2021-10-18 14:27:01 +0200

    add examples

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -27,29 +27,32 @@
 
 unittest_setup()
 {
+  ANSI ansi(&Serial);
+  ansi.print("ANSI_LIB_VERSION: ");
+  ansi.print(ANSI_LIB_VERSION);
 }
+
 
 unittest_teardown()
 {
 }
 
+
 unittest(test_constructor)
 {
-  ANSI ansi(&Serial);
-
   assertEqual(12, ansi.println("1234567890") );
 }
 
+
 unittest(test_gray2color)
 {
-  ANSI ansi(&Serial);
-
   for (int gray = 0; gray < 255; gray += 31)
   {
     int color = ansi.gray2color(gray);
     assertEqual(232 + gray * 3 / 32, color);
   }
 }
+
 
 unittest_main()
 
